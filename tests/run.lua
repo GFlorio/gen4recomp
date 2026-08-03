56a25d6eff4f873c5e753adb9eb6c219bc905273
Gabriel Florio <gabriel@gabrielflorio.com>
2026-08-03 20:46:16 -0300

    maps: add placed-building reference transform

--- tests/run.lua
+++ tests/run.lua
@@ -27,6 +27,7 @@ local MODULES = {
   "tests.nsbtx_test",
   "tests.nsbtx_decoder_opts_test",
   "tests.mesh_compiler_test",
+  "tests.building_transform_test",
   "tests.texture_decoder_test",
   "tests.gx_display_list_test",
   "tests.nsbmd_test",
