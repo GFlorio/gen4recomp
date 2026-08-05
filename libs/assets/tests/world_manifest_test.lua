387e53f606dcf4b0f8eaab59fe292157e7111b78
Gabriel Florio <gabriel@gabrielflorio.com>
2026-08-05 09:17:52 -0300

    assets: add WorldManifest builder for world.lua

---
+++ libs/assets/tests/world_manifest_test.lua
@@ -0,0 +1,30 @@
+local Assert = require("tests.support.Assert")
+local WorldManifest = require("libs.assets.src.WorldManifest")
+
+local T = {}
+
+local function sample()
+  return {
+    { id = 61, symbol = "MAP_NEW_BARK_ELMS_LAB_1F", width = 1, height = 1,
+      matrix = { memberId = 0, x = 0, z = 0 } },
+    { id = 60, symbol = "MAP_NEW_BARK", width = 3, height = 3,
+      matrix = { memberId = 0, x = 21, z = 12 } },
+  }
+end
+
+function T.build_sorts_by_id_and_indexes()
+  local m = WorldManifest.build(sample())
+  Assert.equal(m.maps[1].id, 60)
+  Assert.equal(m.maps[2].id, 61)
+  Assert.equal(m.bySymbol["MAP_NEW_BARK"], 60)
+  Assert.equal(m.byId[61], 2)
+  Assert.equal(m.maps[1].matrix.x, 21)
+end
+
+function T.build_rejects_duplicate_symbol()
+  local dup = sample()
+  dup[1].symbol = "MAP_NEW_BARK"
+  Assert.throws(function() WorldManifest.build(dup) end)
+end
+
+return T
